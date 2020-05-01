96ea1b7856b003ecdb440566964dc0f8bd257c0d
speedglide <speedglide@comcast.net>
2020-05-01 16:24:03 -0400

    Sendin Numbers in hello.php

--- hello.php
+++ hello.php
@@ -4,16 +4,21 @@
     </head>
     <body>
         <?php
-        $note=<<<XML
-        <note>
-        <to>Tove</to>
-        <from>Jani</from>
-        <heading>Reminder</heading>
-        <body>Do not forget me this weekend!</body>
-        </note>
+        $puzzle=<<<XML
+        <puzzleboard>
+            <row0>1,2,3,4,5,6,7,8,9</row0>
+            <row1>1,2,3,4,5,6,7,8,9</row1>
+            <row2>1,2,3,4,5,6,7,8,9</row2>
+            <row3>1,2,3,4,5,6,7,8,9</row3>
+            <row4>1,2,3,4,5,6,7,8,9</row4>
+            <row5>1,2,3,4,5,6,7,8,9</row5>
+            <row6>1,2,3,4,5,6,7,8,9</row6>
+            <row7>1,2,3,4,5,6,7,8,9</row7>
+            <row8>1,2,3,4,5,6,7,8,9</row8>         
+        </puzzleboard>
         XML;
 
-        $xml=new SimpleXMLElement($note);
+        $xml=new SimpleXMLElement($puzzle);
         echo $xml->asXML();
         ?>
 
