117d31f04d6fd6e28edd491c804c2339aa693b70
speedglide <speedglide@comcast.net>
2020-05-16 06:22:30 -0400

    Returns index.html on /
    Issues:
    - If index.html not found, then internal file path returned
    - Statically served files should be kept in a separate folder where access can be controlled
    - custom styled error pages should be returned

--- routes/sudoko.js
+++ routes/sudoko.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
+router.get ('/',function (req, res) {
+  res.sendFile (path.join(__dirname, '../index.html'));
+});
+
+router.get ('/index.html',function (req, res) {
+  res.sendFile (path.join(__dirname, '../index.html'));
+});
 
 router.get ('/sudoko', (req,res)=>{
   res.status(200).json({
